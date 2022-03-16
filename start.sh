a78dd0eb18e47df9047666d51586156bff66638f
MidasVanVeen <midasvanveen.email@gmail.com>
2022-03-16 19:16:45 +0100

    edited start script

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ set -m
 
 cd /tcpwebserver && ./target/release/tcpwebserver -h 0.0.0.0 -p 80 -t 200 &
 
-cd /tictactoe_gameserver && python3 main.py &
+cd /tictactoe_gameserver && python3 main.py
+
+fg %1
