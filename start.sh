8b3ecc580d2c299a88510b59766d5eb741fa261c
MidasVanVeen <midasvanveen.email@gmail.com>
2022-03-17 11:43:41 +0100

    fixed start script

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ set -m
 
 cd /tictactoe_gameserver && python3 main.py &
 
-cd /highscore_server && python3 main.py &
+cd /highscores_recorder && python3 main.py &
 
 cd /tcpwebserver && ./target/release/tcpwebserver -h 0.0.0.0 -p 80 -t 200
 
